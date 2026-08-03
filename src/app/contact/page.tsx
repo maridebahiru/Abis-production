536bea10ed474898589f41f5d6d443f9f1a6f0a6
Mari <maramawitdereje93@gmail.com>
2026-08-03 16:10:34 +0300

    fix: remove blocked Google Map iframe from Contact Us page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -214,20 +214,6 @@ export default function ContactPage() {
               </a>
             </div>
           </div>
-
-          {/* Embedded Google Map */}
-          <div className="rounded-3xl overflow-hidden border border-zinc-800 h-64 relative bg-zinc-950">
-            <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15761.547382741!2d38.7883!3d8.9953!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x164b85cef0ab1555%3A0x889812480!2sBole%20Medhanealem%2C%20Addis%20Ababa!5e0!3m2!1sen!2set!4v1700000000000!5m2!1sen!2set"
-              width="100%"
-              height="100%"
-              style={{ border: 0, filter: 'invert(90%) hue-rotate(180deg)' }}
-              allowFullScreen
-              loading="lazy"
-              referrerPolicy="no-referrer-when-downgrade"
-              title="Abis Production Studio Office Google Map"
-            />
-          </div>
         </div>
       </div>
     </div>
